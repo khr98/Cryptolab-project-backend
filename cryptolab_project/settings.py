a3b43fdcb1e53385f2bc6814c6707cf6bb1a0f1e
hyerim Kim <hyerim980513@gmail.com>
2022-06-23 16:56:45 +0900

    feat: allowed host 설정

--- cryptolab_project/settings.py
+++ cryptolab_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--d0iqe$*h%7a&&je4u)%wxmdn60eadex*nk(a22y28u1r5t9d1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
