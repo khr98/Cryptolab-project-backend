54f2f83143e3a335496c65750fa92de3c975975d
hyerim Kim <hyerim980513@gmail.com>
2022-07-05 00:33:06 +0900

    feat: generics.CreateAPIView를 사용한 회원가입기능 만들기

--- user/serializers.py
+++ user/serializers.py
@@ -1,14 +1,82 @@
 from rest_framework import serializers
-from .models import User,Qrcode
+from .models import *
+from django.contrib.auth import get_user_model
+from rest_framework.validators import UniqueValidator
 
 
-class UserSerializer(serializers.ModelSerializer):
+
+class UserCreateSerializer(serializers.ModelSerializer) :
+    username = serializers.CharField(min_length=4, max_length=30)
+    password = serializers.CharField(min_length=4, max_length=12, write_only=True)
+    email = serializers.EmailField(validators=[
+        UniqueValidator(queryset=get_user_model().objects.all(), message="이미 존재하는 이메일 입니다.")
+    ])
+    
+    # def create(self,validated_data):
+    #     password = validated_data.pop('password',None)
+    #     instance = self.Meta.model(**validated_data)
+    #     if password is not None :
+    #     #provide django, password will be hashing!
+    #         instance.set_password(password)
+    #     instance.save()
+    #     return instance
+    
+    def create(self, validated_data):
+            user = get_user_model().objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+        )
+            return user
+    
     class Meta:
-        model=User
-        fields=('email','password')
+        model = get_user_model()
+        fields = ("username", "email", "password","created_at")
+
+
+
+
+
+# User = get_user_model()
+
+# class UserCreateSerializer(serializers.ModelSerializer):
+#     username = serializers.CharField(min_length=4, max_length=30)
+#     password = serializers.CharField(min_length=4, max_length=12, write_only=True)
+#     email = serializers.EmailField(validators=[
+#         UniqueValidator(queryset=get_user_model().objects.all(), message="이미 존재하는 이메일 입니다.")
+#     ])
+
+#     def create(self, validated_data):
+#         user = get_user_model().objects.create_user(
+#             username=validated_data['username'],
+#             email=validated_data['email'],
+#             password=validated_data['password']
+#         )
+#         return user
+
+#     class Meta:
+#         model = get_user_model()
+#         fields = ("username", "email", "password",)
+
+# class UserSerializer(serializers.ModelSerializer) :
+#     class Meta:
+#         model = User
+#         fields = ['username', 'email', 'password', 'created_at']
+#         extra_kwargs = {
+#             'password' : {'write_only' : True}
+#         }
+
+#     def create(self, validated_data):
+#         password = validated_data.pop('password', None)
+#         instance = self.Meta.model(**validated_data)
+#         if password is not None :
+#             instance.set_password(password)
+#         instance.save()
+#         return instance
+
   
 class QrCodeSerializer(serializers.ModelSerializer):
 	class Meta:
 		model=Qrcode
-		fields=('user','latitude','longitude')
+		fields= '__all__'
   
