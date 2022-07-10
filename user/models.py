0ea9956993510d43a9700170ad3e072817559516
hyerim Kim <hyerim980513@gmail.com>
2022-07-11 01:47:30 +0900

    feat: 회원가입 기능 추가

--- user/models.py
+++ user/models.py
@@ -1,19 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.utils import timezone
-from django.utils.translation import ugettext_lazy as _
+
 
 class User(AbstractUser):
-    email = models.EmailField(_("email address"), blank=False, unique=True)
-    created_at = models.DateTimeField(auto_now_add=True,null=True)
-    REQUIRED_FIELDS = ["email", "password"]
+    email = models.EmailField(verbose_name="email_address", blank=False, unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
     class Meta:
         db_table = 'User'
 
 class Qrcode(models.Model):
     seqId = models.AutoField(db_column='idQRcode', primary_key=True)  # Field name made lowercase.
-    #user = models.ForeignKey('User',models.DO_NOTHING,max_length=45, blank=True, null=True, db_column='user')
+    user = models.ForeignKey('User',models.DO_NOTHING,blank=True, null=True, db_column='user')
     latitude = models.FloatField(blank=True, null=True)
     longitude = models.FloatField(blank=True, null=True)
 
--- user/serializers.py
+++ user/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import *
 from django.contrib.auth import get_user_model
 from rest_framework.validators import UniqueValidator
+from rest_framework.response import Response
+from django.core.mail import send_mail
 
 
 
@@ -12,69 +14,21 @@ class UserCreateSerializer(serializers.ModelSerializer) :
         UniqueValidator(queryset=get_user_model().objects.all(), message="이미 존재하는 이메일 입니다.")
     ])
     
-    # def create(self,validated_data):
-    #     password = validated_data.pop('password',None)
-    #     instance = self.Meta.model(**validated_data)
-    #     if password is not None :
-    #     #provide django, password will be hashing!
-    #         instance.set_password(password)
-    #     instance.save()
-    #     return instance
-    
     def create(self, validated_data):
             user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password'],
+            is_active = False,
         )
             return user
+
     
     class Meta:
         model = get_user_model()
-        fields = ("username", "email", "password","created_at")
+        fields = ("username", "email", "password")
 
 
-
-
-
-# User = get_user_model()
-
-# class UserCreateSerializer(serializers.ModelSerializer):
-#     username = serializers.CharField(min_length=4, max_length=30)
-#     password = serializers.CharField(min_length=4, max_length=12, write_only=True)
-#     email = serializers.EmailField(validators=[
-#         UniqueValidator(queryset=get_user_model().objects.all(), message="이미 존재하는 이메일 입니다.")
-#     ])
-
-#     def create(self, validated_data):
-#         user = get_user_model().objects.create_user(
-#             username=validated_data['username'],
-#             email=validated_data['email'],
-#             password=validated_data['password']
-#         )
-#         return user
-
-#     class Meta:
-#         model = get_user_model()
-#         fields = ("username", "email", "password",)
-
-# class UserSerializer(serializers.ModelSerializer) :
-#     class Meta:
-#         model = User
-#         fields = ['username', 'email', 'password', 'created_at']
-#         extra_kwargs = {
-#             'password' : {'write_only' : True}
-#         }
-
-#     def create(self, validated_data):
-#         password = validated_data.pop('password', None)
-#         instance = self.Meta.model(**validated_data)
-#         if password is not None :
-#             instance.set_password(password)
-#         instance.save()
-#         return instance
-
-  
 class QrCodeSerializer(serializers.ModelSerializer):
 	class Meta:
 		model=Qrcode
